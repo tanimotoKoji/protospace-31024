9c55ebe1c62d8b8aa7c6d2610328d3497d803965
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-14 11:29:58 +0900

    他ユーザーがURLで編集ページに行くのを防ぐ記述追加
    すいません。提出後に漏れに気付きました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,6 +28,9 @@ class PrototypesController < ApplicationController
 
     def edit
       @prototype = Prototype.find(params[:id])
+      unless user_signed_in? && current_user.id == @prototype.user_id
+        redirect_to action: :index
+      end
     end
 
     def update
